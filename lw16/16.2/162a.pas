a7ee1abc0f5e76586334bd2d44494d4c5346854b
ktodkf <62442562+ktodkf@users.noreply.github.com>
2020-03-31 07:52:12 +0300

    Add files via upload

---
+++ lw16/16.2/162a.pas
@@ -0,0 +1,33 @@
+PROGRAM SarahRevere(INPUT, OUTPUT); 
+VAR
+  W1, W2, W3, W4: CHAR;
+  Looking, Land, Sea: BOOLEAN;  
+BEGIN {SarahRevere}   
+  {»нициализаци€}
+   W2:=' ';                                                                           
+   W3:=' ';                                                                           
+   W4:=' ';                                                                                                                                              
+   Looking:= TRUE;
+   Sea:= FALSE;
+   Land:= FALSE;
+   WHILE Looking AND NOT (Land OR Sea)   
+   DO
+     BEGIN
+      {движение окна}
+       BEGIN
+         W1:= W2;
+         W2:= W3;
+         W3:= W4;
+         READ(W4);
+         Looking:= (W4 <> '#')
+       END;
+       {проверка окна на land}        
+       Land := (W1 = 'l') AND (W2 = 'a') AND (W3 = 'n') AND (W4 = 'd'); 
+     END;  
+     IF Land 
+     THEN
+       WRITELN('Land')
+     ELSE 
+       WRITELN('SARAH DID NOT SAY')   
+END.{SARAHREVERE}
+
