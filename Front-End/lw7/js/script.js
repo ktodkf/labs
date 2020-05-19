7087ce2f8a55c9b19535ca690eed6cb89cbd496d
ktodkf <62442562+ktodkf@users.noreply.github.com>
2020-05-19 21:17:32 +0300

    Add files via upload

---
+++ Front-End/lw7/js/script.js
@@ -0,0 +1,51 @@
+function isPrimeDigit(digit){
+    let isPrime;
+    if (typeof(digit) == 'number'){
+     combine(isPrime, digit);
+    }
+    else if (typeof(digit) == 'object'){
+      if (digit.length == 0){
+        console.log('Некорректный ввод');
+      }
+      else{
+        for(let i = 0; i < digit.length; i++){
+          let num = digit[i];
+          let isPrime;
+          combine(isPrime, num);
+        }
+      }
+    }
+    else{
+      console.log('Некорректный ввод')
+    }
+  }
+  
+  function isPrimeCheck(isPrime, digit){
+    for (let i = 2; i < digit; i++){
+    isPrime = true;
+    if ((digit % i) == 0){
+      isPrime = false;
+      break;
+    }
+   }
+    return isPrime;
+  }
+  
+  function primePrint(isPrime, digit){
+    if (isPrime){
+      console.log(digit + ' простое число');
+    }
+    else{
+     console.log(digit + ' непростое число');
+    }
+  }
+
+  function combine(isPrime, digit) {
+    isPrime = true;
+    if (digit < 2){
+      console.log(digit + ' непростое число');
+    } else {
+      isPrime = isPrimeCheck(isPrime, digit);
+      primePrint(isPrime, digit);
+    }
+  }
