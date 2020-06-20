331a2c6a9431771c39f47fe30d8b66f4e4e8cc35
ktodkf <62442562+ktodkf@users.noreply.github.com>
2020-06-20 14:52:44 +0300

    Update script.js

--- Front-End/lw7/js/script.js
+++ Front-End/lw7/js/script.js
@@ -1,49 +1,54 @@
-function isPrimeNumber(digit){
- if (typeof(digit) == 'number'){
-     let isPrime = isPrimeResult(digit);
-    }
-    else if (typeof(digit) == 'object'){
-      if (digit.length == 0){
-        console.log('Некорректный ввод');
-      }
-      else{
-        for(let i = 0; i < digit.length; i++){
+function isPrimeNumber(digit) {
+  if (typeof (digit) == 'number') {
+    let isPrime = isPrimeResult(digit);
+  } 
+  else if (typeof (digit) == 'object') {
+    if (digit.length == 0) {
+      console.log('Некорректный ввод');
+    } 
+    else {
+      for (let i = 0; i < digit.length; i++) {
+        if (typeof (digit[i]) == 'number') {
           let num = digit[i];
           let isPrime = isPrimeResult(num);
+        } 
+        else {
+          console.log('Некорректный ввод');
+        }
       }
-    } 
-  }   
-    else{
-      console.log('Некорректный ввод')
     }
+  } 
+  else {
+    console.log('Некорректный ввод');
   }
-  
-  function isPrimeDigit(digit){
-    for (let i = 2; i < digit; i++){
+}
+
+function isPrimeDigit(digit) {
+  for (let i = 2; i < digit; i++) {
     isPrime = true;
-    if ((digit % i) == 0){
+    if ((digit % i) == 0) {
       isPrime = false;
       break;
     }
-   }   
-   if (digit < 2){
-      isPrime = false;
-    }  
-    return isPrime;
   }
-  
-  function isPrimePrint(isPrime, digit){
-    if (isPrime){
-      console.log(digit + ' простое число');
-    }
-    else{
-     console.log(digit + ' непростое число');
-    }
+  if (digit < 2) {
+    isPrime = false;
   }
+  return isPrime;
+}
 
-  function isPrimeResult(digit) {
-      isPrime = true;
-      isPrime = isPrimeDigit (digit);
-      isPrimePrint(isPrime, digit);
-      return isPrime;
+function isPrimePrint(isPrime, digit) {
+  if (isPrime) {
+    console.log(digit + ' простое число');
+  } 
+  else {
+    console.log(digit + ' непростое число');
+  }
+}
+
+function isPrimeResult(digit) {
+  isPrime = true;
+  isPrime = isPrimeDigit(digit);
+  isPrimePrint(isPrime, digit);
+  return isPrime;
 }
